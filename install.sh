4ed367a8a94f738590604527b5fd6212c83e18e0
Alexander Phillipson <127406648+AJPNetworks@users.noreply.github.com>
2023-07-06 06:16:58 -0400

    Update install.sh
    Allowed for the install script to be used as a repair tool.  Simply run it again and it will save all the uploaded icons and background and do a full fresh install .. minus the database, that doesn't get touched by the script

--- install.sh
+++ install.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 sudo mkdir /var/log/dashlinx
 sudo touch /var/log/dashlinx/install.log
+sudo touch /var/log/dashlinx/database_password.log
+sudo chmod 755 /var/log/dashlinx
 
 log_file="/var/log/dashlinx/install.log"
+dbpsw="/var/log/dashlinx/database_password.log"
 
 
 # Update the server
 echo "Updating the server..."
-sudo apt update
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
+sudo apt update | sudo tee -a "$log_file" >/dev/null
+sudo apt upgrade -y | sudo tee -a "$log_file" >/dev/null
+sudo apt dist-upgrade -y | sudo tee -a "$log_file" >/dev/null
 echo "Server Updated"
 
 # Install LEMP stack
 echo "Installing necessary packages"
 echo "Installing nginx"
-sudo apt install nginx -y
+sudo DEBIAN_FRONTEND=noninteractive apt install nginx -y | sudo tee -a "$log_file" >/dev/null
 echo "Installing mysql-server"
-sudo apt install mysql-server -y
+sudo DEBIAN_FRONTEND=noninteractive apt install mysql-server -y | sudo tee -a "$log_file" >/dev/null
 echo "Installing php8.1-fpm"
-sudo apt install php8.1-fpm -y
+sudo DEBIAN_FRONTEND=noninteractive apt install php8.1-fpm -y | sudo tee -a "$log_file" >/dev/null
 echo "Installing php8.1-mysql"
-sudo apt install php8.1-mysql -y
+sudo DEBIAN_FRONTEND=noninteractive apt install php8.1-mysql -y | sudo tee -a "$log_file" >/dev/null
 echo "Installing Git"
-sudo apt install git -y
+sudo DEBIAN_FRONTEND=noninteractive apt install git -y | sudo tee -a "$log_file" >/dev/null
 echo "Installing php-fpm"
-sudo apt install php-fpm -y
+sudo DEBIAN_FRONTEND=noninteractive apt install php-fpm -y | sudo tee -a "$log_file" >/dev/null
 echo "All packages installed"
 
 
@@ -33,7 +36,15 @@ echo "All packages installed"
 
 title="DashLinx"
 tab="DashLinx"
+
 password=$(openssl rand -base64 6 | tr -d '+/' | cut -c1-8)
+
+if [ -f "$dbpsw" ]; then
+  password=$(sudo cat "$dbpsw")
+  echo "Found existing password for database"
+fi
+
+
 port="80"
 
 # Parse command-line options and arguments
@@ -59,11 +70,24 @@ while getopts "t:b:P:p:" opt; do
 done
 
 echo "Cleaning Nginx"
-sudo rm -R /var/www/html/*
+
+
+if [ -d "/var/www/html/src/images/uploaded" ]; then
+  sudo rm -R "/tmp/uploaded"
+  sudo rm "/tmp/background.png"
+  sudo cp -R "/var/www/html/src/images/uploaded" "/tmp/uploaded"
+  sudo cp "/var/www/html/src/images/background.png" "/tmp/background.png"
+  sudo rm -R /var/www/html/* | sudo tee -a "$log_file" >/dev/null
+  restore=true
+else
+  restore=false
+  echo "No images to restore"
+fi
 echo "Done"
 
+
 echo "Downloading DashLinx.."
-git clone https://github.com/AJPNetworks/DashLinx.git
+git clone https://github.com/AJPNetworks/DashLinx.git | sudo tee -a "$log_file" >/dev/null
 sudo cp -R DashLinx/app/* /var/www/html
 sudo rm -R DashLinx
 echo "Downloaded"
@@ -81,8 +105,14 @@ echo "<?php
 ?>" | sudo tee /var/www/html/setup.php >/dev/null
 echo "Configured"
 
-
-
+if [ "$restore" = true ]; then 
+  sudo cp -R "/tmp/uploaded" "/var/www/html/src/images"
+  sudo cp  "/tmp/background.png" "/var/www/html/src/images/background.png"
+  sudo rm -R "/tmp/uploaded"
+  sudo rm -R "/var/www/html/src/images/uploaded/uploaded"
+  sudo rm "/tmp/background.png"
+  echo "Images Restored"
+fi
 
 
 echo "Configuring Nginx"
@@ -108,7 +138,7 @@ if [ "$nginx_conf_file" != "Nginx configuration file not found." ]; then
   # Set the desired port number
 
   # Use sed to change the port in the configuration file
-  sudo sed -i "s/^listen .*;$/listen $port;/" "$nginx_conf_file"
+  sudo sed -i "s/^listen .*;$/listen $port;/" "$nginx_conf_file" | sudo tee -a "$log_file" >/dev/null
 
   echo "Nginx port changed to $port."
 else
@@ -120,7 +150,7 @@ fi
 PHP_VERSION=$(php -r 'echo PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;')
 
 # Configure PHP-FPM
-sudo sed -i "s|listen = /run/php/php.*-fpm.sock|listen = /run/php/php${PHP_VERSION}-fpm.sock|" /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
+sudo sed -i "s|listen = /run/php/php.*-fpm.sock|listen = /run/php/php${PHP_VERSION}-fpm.sock|" /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf | sudo tee -a "$log_file" >/dev/null
 
 # Configure Nginx
 sudo tee /etc/nginx/sites-available/default >/dev/null <<EOT
@@ -150,9 +180,9 @@ sudo service nginx restart
 echo "Configured"
 
 echo "Creating Database"
-sudo mysql -e "CREATE DATABASE dashlinx;"
-sudo mysql -e "CREATE USER 'dashlinx'@'localhost' IDENTIFIED BY '$password';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON dashlinx.* TO 'dashlinx'@'localhost';"
+sudo mysql -e "CREATE DATABASE dashlinx;" | sudo tee -a "$log_file" >/dev/null
+sudo mysql -e "CREATE USER 'dashlinx'@'localhost' IDENTIFIED BY '$password';" | sudo tee -a "$log_file" >/dev/null
+sudo mysql -e "GRANT ALL PRIVILEGES ON dashlinx.* TO 'dashlinx'@'localhost';" | sudo tee -a "$log_file" >/dev/null
 echo "Created"
 
 
@@ -165,20 +195,20 @@ echo "Reloaded"
 
 # Start and enable Nginx
 echo "Starting Nginx"
-sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo systemctl start nginx | sudo tee -a "$log_file" >/dev/null
+sudo systemctl enable nginx | sudo tee -a "$log_file" >/dev/null
 echo "Started"
 
 # Start and enable MySQL
 echo "Starting MySQL"
-sudo systemctl start mysql
-sudo systemctl enable mysql
+sudo systemctl start mysql | sudo tee -a "$log_file" >/dev/null
+sudo systemctl enable mysql | sudo tee -a "$log_file" >/dev/null
 echo "Started"
 
 # Start and enable PHP-FPM
 echo "Starting PHP"
-sudo systemctl start php8.1-fpm
-sudo systemctl enable php8.1-fpm
+sudo systemctl start php8.1-fpm | sudo tee -a "$log_file" >/dev/null
+sudo systemctl enable php8.1-fpm | sudo tee -a "$log_file" >/dev/null
 echo "Started"
 
 echo "Setting permissions"
@@ -190,4 +220,5 @@ sudo service nginx restart
 
 echo "Nginx is listening on port $port"
 echo "INSTALLED"
+ip_address=$(ip -4 route get 8.8.8.8 | awk '/src/ {print $7}')
 echo "You can now go to http://$ip_address:$port and configure DashLinx!"
