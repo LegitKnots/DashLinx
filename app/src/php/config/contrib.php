7c52ee16814d4fe667b8dab5e867f12c56096ce4
Alexander Phillipson <127406648+AJPNetworks@users.noreply.github.com>
2023-07-10 11:57:56 -0400

    Styling
    Updated some styling mishaps on mobile and overlooked a the file location for contrib button icons

--- app/src/php/config/contrib.php
+++ app/src/php/config/contrib.php
@@ -1,4 +1,4 @@
 <div class="contrib-content">
-		<div class='contrib-button' onclick='window.open("https://paypal.me/kn0t5")'><img src="../src/images/paypal.png"><h1>PayPal</h1></div>
-		<div class='contrib-button' onclick='window.open("https://github.com/AJPNetworks/")'><img src="../src/images/github.png"><h1>Github</h1></div>
+		<div class='contrib-button' onclick='window.open("https://paypal.me/kn0t5")'><img src="../src/images/icons/paypal.png"><h1>PayPal</h1></div>
+		<div class='contrib-button' onclick='window.open("https://github.com/AJPNetworks/")'><img src="../src/images/icons/github.png"><h1>Github</h1></div>
 </div>
